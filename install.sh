808cacc38210eb1615e955a4053862234b60ea6f
xianhuawei <xianhuawei.zhcn@qq.com>
2016-08-22 21:44:24 +0800

    install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-sudo yum install -y git redis python-pip python-simplejson python-requests MySQL-python python-lxml libvirt-python libxml2-python python-dateutil net-snmp net-snmp-utils net-snmp-python
+sudo yum install -y git redis python-pip python-simplejson python-requests MySQL-python python-lxml libvirt-python libxml2-python python-dateutil
 sudo pip install --upgrade pip
-sudo pip install gevent ncclient docker-py kazoo falcon peewee gunicorn pyzabbix redis kazoo pika kafka-python autojenkins upyun ldap3 ansible
+sudo pip install gevent ncclient docker-py kazoo falcon peewee gunicorn pyzabbix redis kazoo pika kafka-python autojenkins upyun ldap7 ansible libxml2-python elasticsearch libvirt-python simplejson requests python-dateutil lxml
 
 service redis start
 
